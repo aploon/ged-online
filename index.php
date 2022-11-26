c9480821492f725d9a949be63f642e171d67697b
Arnaud <arnaudadjovi274@gmail.com>
2022-11-26 20:32:56 +0100

    Commit de modification et de test

--- index.php
+++ index.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>GED-ELYON TEST</title>
 </head>
 <body>
     <h1>Bienvenu sur l'app de test du logiciel GED-ELYON</h1>
+    <ul>
+        <li class="export"><a href="export">Exportation en pdf</a></li>
+    </ul>
 </body>
 </html>
