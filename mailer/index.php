9b2220a47800e4970caac28ab238c3ef05ac9435
Arnaud <arnaudadjovi274@gmail.com>
2022-12-19 12:43:11 +0100

    Ajout de phpmailer et example

--- mailer/index.php
+++ mailer/index.php
@@ -1,15 +1,15 @@
 <?php
 //Import PHPMailer classes into the global namespace
 //These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
+// use PHPMailer\PHPMailer\PHPMailer;
+// use PHPMailer\PHPMailer\SMTP;
+// use PHPMailer\PHPMailer\Exception;
 
-//Load Composer's autoloader
-require 'vendor/autoload.php';
+// //Load Composer's autoloader
+// require 'vendor/autoload.php';
 
-//Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
+// //Create an instance; passing `true` enables exceptions
+// $mail = new PHPMailer(true);
 
 // try {
 //     //Server settings
