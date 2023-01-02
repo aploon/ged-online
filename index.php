5728bb92a2d650c047db24ed8beff6445c70c137
Arnaud <arnaudadjovi274@gmail.com>
2023-01-02 11:49:21 +0100

    gitignore update for ignore /elfinder/files

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GED-ELYON TEST</title>
+    <title>GED-ELYON TEST ONLINE</title>
 </head>
 <body>
     <h1>Bienvenu sur l'app de test du logiciel GED-ELYON</h1>
